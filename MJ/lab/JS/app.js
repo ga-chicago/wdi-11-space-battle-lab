e9ef8d545009a95fb8520cc2f7d5c7f19a3cac43
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-03-23 02:31:36 -0500

    Completed Logic if Hit

--- MJ/lab/JS/app.js
+++ MJ/lab/JS/app.js
@@ -29,11 +29,81 @@ class AlienShipFactory{
 //alien ready to generate
 const alien = new AlienShipFactory();
 
-const ussAssemblyShip = {
-	hull: 20,
-	firepower: 5,
-	accuracy:.7
-};
+
+
+
+//Object for game in general
+const spaceBattleGame = {
+
+	//USS Ship Object
+	ussAssemblyShip:{
+		hull: 20,
+		firepower: 5,
+		accuracy:.7
+	},
+
+
+	//how many aliens to battle based on user input
+	howManyAliens(parameter){
+		//generate aliens using factory
+		for(let i = 0; i < parameter; i ++){
+			alien.generateShip();
+		}
+		//display alien ships
+		console.log(aliens.alienShipArray);
+
+	},
+
+
+
+	ussAttack(){
+		//prompt user to fire laser or retreat
+		let fireRetreat = window.prompt('Fire Missile? (Y) or Retreat? (R)');
+
+		if(fireRetreat === 'Y'|| fireRetreat ==='y'){
+			//Continue Battle
+			//USS Fires Missile At Alien
+			//If USS Hits Alien Ship
+			if(Math.random() > this.ussAssemblyShip.accuracy){
+				//decrement alien health if hit
+				alien.alienShipArray[0].hull -= this.ussAssemblyShip.firepower;
+				//if alien has no health remaining, remove from array, if alien is alive, then alien attacks
+				(alien.alienShipArray[0].hull <= 0) ? alienDecrement() : alienAttack()
+			}
+
+		}
+
+		else if (fireRetreat === 'R' || fireRetreat === 'r'){
+			gameOver()
+		}
+		
+
+	}
+	alienAttack(){
+		//Alien attacks
+		//If Alien Hits Target
+		if(Math.random() > alien.alienShipArray[0].accuracy){
+			//decrement health of USS
+			this.ussAssemblyShip.hull -= alienShipArray[0].firepower;
+			//If USS is Alive, Uss Attacks, Else Game Over
+			(this.ussAssemblyShip.hull <= 0) ? gameOver() : ussAttack()
+		}
+
+	}
+	alienDecrement(){
+		//Remove Dead Alien From Array
+		alien.alienShipArray.splice(0,1);
+
+		console.log('You Killed An Alien!');
+		//Attack Next Alien
+		ussAttack()
+
+	}
+	
+	gameOver(){
+
+	}
+}
 
 
 
